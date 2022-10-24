3fff2888c1d9730f01aaa678c3eb352434622a9b
Abdel <abdelbrahim945@gmail.com>
2022-10-23 20:43:05 -0700

    h

--- libuthread/queue.c
+++ libuthread/queue.c
@@ -67,6 +67,7 @@ int queue_dequeue(queue_t queue, void **data) // ptr= null
 		
 	return 0;
 }
+/*
 
 int queue_delete(queue_t queue, void *data) 
 {
@@ -99,6 +100,55 @@ int queue_length(queue_t queue)
 
 	return queue->count;
 	
+
+}*/
+
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+#define TEST_ASSERT(assert)				\
+do {									\
+	printf("ASSERT: " #assert " ... ");	\
+	if (assert) {						\
+		printf("PASS\n");				\
+	} else	{							\
+		printf("FAIL\n");				\
+		exit(1);						\
+	}									\
+} while(0)
+
+/* Create */
+void test_create(void)
+{
+	fprintf(stderr, "*** TEST create ***\n");
+
+	TEST_ASSERT(queue_create() != NULL);
 }
 
+/* Enqueue/Dequeue simple */
+void test_queue_simple(void)
+{
+	int data = 3, *ptr;
+	queue_t q;
+
+	fprintf(stderr, "*** TEST queue_simple ***\n");
+
+	q = queue_create();
+	queue_enqueue(q, &data);
+	queue_dequeue(q, (void**)&ptr);
+	TEST_ASSERT(ptr == &data);
+}
+
+int main(void)
+{
+	test_create();
+	test_queue_simple();
+
+
+	return 0;
+}
 
